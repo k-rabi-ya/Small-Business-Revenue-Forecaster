b61989513ff42f9e1f23d331a44860daf5c6226c
=Srishanth Gowda <srishanthgowda03@gmail.com>
2026-03-02 19:16:11 +0530

    SGD reggessor

--- regression_model.py
+++ regression_model.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.linear_model import SGDRegressor
 
 # Load dataset
 data = pd.read_csv("data/cleaned_panel_data.csv")
@@ -62,3 +63,25 @@ print("MAE:", mae)
 print("MSE:", mse)
 print("RMSE:", rmse)
 print("R2 Score:", r2)
+
+# SGD Regressor
+
+sgd = SGDRegressor(max_iter=1000, tol=1e-3, random_state=42)
+
+# Train on scaled data
+sgd.fit(X_train_scaled, y_train)
+
+# Predictions
+y_pred_sgd = sgd.predict(X_test_scaled)
+
+# Evaluation
+mae_sgd = mean_absolute_error(y_test, y_pred_sgd)
+mse_sgd = mean_squared_error(y_test, y_pred_sgd)
+rmse_sgd = np.sqrt(mse_sgd)
+r2_sgd = r2_score(y_test, y_pred_sgd)
+
+print("\nSGD Regressor Results:")
+print("MAE:", mae_sgd)
+print("MSE:", mse_sgd)
+print("RMSE:", rmse_sgd)
+print("R2 Score:", r2_sgd)
